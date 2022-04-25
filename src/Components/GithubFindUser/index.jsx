a7b718e76802631f0b10c594bf5fbf75c3a7656b
Vladislav <simwebkz@gmail.com>
2022-04-25 15:46:53 +0600

    axios & custom hooks add

--- src/Components/GithubFindUser/index.jsx
+++ src/Components/GithubFindUser/index.jsx
@@ -1,8 +1,14 @@
 import classNames from "classnames/bind";
+import useFetch from "../../CustomHooks/useFetch";
 import styles from "./github-find-user.module.scss";
 
 const GithubFindUser = () => {
   const cx = classNames.bind(styles);
+  const {
+    data: user,
+    isPending,
+    error,
+  } = useFetch("https://api.github.com/users/rubyhat");
   return <div className="github-find">github init</div>;
 };
 
---
+++ src/CustomHooks/useFetch/index.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null),
+    [isPending, setIsPending] = useState(true),
+    [error, setError] = useState(null);
+
+  useEffect(() => {
+    const startTime = Date.now();
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    // cleanup useEffect
+    const abortCont = new AbortController();
+
+    axios({
+      url,
+      method: "GET",
+      signal: abortCont.signal,
+      headers: headers,
+    })
+      .then((res) => {
+        console.log("Axios URL: ", url);
+        if (res.status !== 200) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        let nowTime = Date.now();
+        let seconds = Math.floor((nowTime - startTime) / 1000);
+        let milliseconds = Math.floor((nowTime - startTime) % 1000);
+        console.log(
+          `Load time ${seconds}.${milliseconds} seconds of ${res.config.url}`
+        );
+        console.log("Axios response: ", res);
+        return res.data;
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          console.log("Axios Error: ", error);
+          setIsPending(false);
+          setError(error.message);
+        }
+      });
+
+    return () => abortCont.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
