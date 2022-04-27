9868f37e821f5db6a801c83796c978a0ec42c456
Vladislav <simwebkz@gmail.com>
2022-04-27 14:42:55 +0600

    bubble sort

--- src/Components/Algorithms/Items/04_bubble_sort.js
+++ src/Components/Algorithms/Items/04_bubble_sort.js
@@ -1,4 +1,4 @@
-const selectionSort = () => {
+const bubbleSort = () => {
   const array = [
     0, 3, 2, 5, 6, 8, 1, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6,
     3, 32,
@@ -7,26 +7,24 @@ const selectionSort = () => {
 
   const sort = (array) => {
     for (let i = 0; i < array.length; i++) {
-      let minIndex = i;
-      for (let k = i + 1; k < array.length; k++) {
-        if (array[k] < array[minIndex]) {
-          minIndex = k;
+      for (let k = 0; k < array.length; k++) {
+        if (array[k + 1] < array[k]) {
+          let temp = array[k];
+          array[k] = array[k + 1];
+          array[k + 1] = temp;
         }
         count += 1;
       }
-      let temp = array[i];
-      array[i] = array[minIndex];
-      array[minIndex] = temp;
     }
     return array;
   };
 
-  console.log("3. Сортировка выбором");
+  console.log("4. Сортировка пузырьком");
   console.log("Массив: ", array);
   console.log("Отсортированный массив: ", sort(array));
   console.log("Количество иттераций: ", count);
-  console.log("Длина массива: ", array.length); // O(1/2n^2)
+  console.log("Длина массива: ", array.length); // O(n^2)
   console.log("\n");
 };
 
-export default selectionSort;
+export default bubbleSort;
--- src/Components/Algorithms/index.jsx
+++ src/Components/Algorithms/index.jsx
@@ -2,11 +2,13 @@ import bigO from "./images/big-o.webp";
 import linearSearch from "./Items/01_linear";
 import binarySearch from "./Items/02_binary";
 import selectionSort from "./Items/03_selection_sort";
+import bubbleSort from "./Items/04_bubble_sort";
 
 const Algorithms = () => {
   linearSearch();
   binarySearch();
   selectionSort();
+  bubbleSort();
   return (
     <div className="algorithms">
       <h1 className="text-3xl font-bold text-rose-700">Hello, algorithms!</h1>
