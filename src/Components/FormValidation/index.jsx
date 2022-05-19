89b8547d14f364704a0a55d6eb920554132c9522
Vladislav <simwebkz@gmail.com>
2022-05-19 12:19:23 +0600

    form created

--- src/Components/FormValidation/index.jsx
+++ src/Components/FormValidation/index.jsx
@@ -1,5 +1,42 @@
 const FormValidation = () => {
-  return <div className="hello">hello form</div>;
+  return (
+    <div className="form-validation">
+      <div className="columns">
+        <div className="column is-one-third">
+          <form className="form">
+            <h3 className="title">Form</h3>
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+                <input
+                  name="email"
+                  class="input"
+                  type="text"
+                  placeholder="your_email@mail.com"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input
+                  name="password"
+                  class="input"
+                  type="password"
+                  placeholder="Type your password"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <div class="control">
+                <button class="button is-link">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FormValidation;
